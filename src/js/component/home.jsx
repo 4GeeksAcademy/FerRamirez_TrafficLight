9336cac8b054e2d3aaca6f00e69d0f1d572e271b
Fernando Ramirez <86069304+ferrami@users.noreply.github.com>
2023-06-08 19:08:23 +0000

    Completed_v1

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,31 @@
-import React from "react";
+import React, {useState} from "react";
+import style from "./home.module.css"
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+// import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [ color, setColor] = useState("");
+
+	const  trafficLight = (color) => {
+		setColor(color)
+	}
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<div className="mt-4 p-5 text-white rounded">
+			<div className={style.header}>
+				<div className="stick bg-dark mb-5 rounded">
+				</div>
+				<div className="d-flex justify-content-center align-items-center">
+					<div className="d-flex-column justify-content-center align-items-center mt-4 p-2 bg-dark text-white shadow-lg rounded">
+						<div onMouseOver={() => trafficLight("red")} onMouseOut={() => trafficLight("none")} className={color === "red" ? style.lightOnRed : style.lightOffRed}></div>
+						<div onMouseOver={() => trafficLight("yellow")} onMouseOut={() => trafficLight("none")} className={color === "yellow" ? style.lightOnYellow : style.lightOffYellow}></div>
+						<div onMouseOver={() => trafficLight("green")} onMouseOut={() => trafficLight("none")} className={color === "green" ? style.lightOnGreen : style.lightOffGreen}></div>
+					</div>
+				</div>
+			</div>
+		</div>	
 	);
 };
 
